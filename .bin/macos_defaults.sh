20d6ffc9981995a45d05ad928eff116ef5367ac8
takecchi <takecchi.kobayashi@gmail.com>
2026-01-30 15:59:54 +0900

    gitconfigにsigningkeyの追加 macos_defaults変更

--- .bin/macos_defaults.sh
+++ .bin/macos_defaults.sh
@@ -11,5 +11,9 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 # Finder: 拡張子を表示
 defaults write -g AppleShowAllExtensions -bool true
 
+# Finder: サイドバーにハードディスクを表示
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
+
 # Finderの設定を再起動して変更を適用
 killall Finder &> /dev/null
